0293ccc8d166cbd4f4b6137be3ca3cb5a9da31cb
Gemuel <Gemuel20sampayan@gmail.com>
2023-07-18 12:28:39 +0800

    Adding Request to POSTMAN

--- config/db.js
+++ config/db.js
@@ -4,13 +4,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db,{
-            useNewUrlParser: true
-        });
+        await mongoose.connect(db); // if You have a issue you add (db, {useNewUrlParser: true})
         console.log('MongoDb is Connected');
     }catch(err) {
         console.log(err.message);
-        process.exit(1); 
+        process.exit(1);
     }
 }
 
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    avatar: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Change const User = ... to let User = ... or var User = ...
+let User = mongoose.model('users', UserSchema);
+
+module.exports = User;
