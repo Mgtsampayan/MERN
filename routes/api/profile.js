ee507f4c9e9ae9a9fb92fe8d50f1c41b24025deb
Gemuel <Gemuel20sampayan@gmail.com>
2023-07-31 22:31:50 +0800

    Adding Get User Id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -100,4 +100,19 @@ router.post("/", [auth, profileValidation], async (req, res) => {
     }
 });
 
+// GET api/profile
+// Get all profiles
+// Access Public
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        if (!profile) return res.status(400).json({ msg: 'There is no profile for this user' });
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
 module.exports = router;
