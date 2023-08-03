4c5fe237631b1626ddde92d98d3b38b6108089c1
Gemuel <Gemuel20sampayan@gmail.com>
2023-08-03 23:21:29 +0800

    Some Adding for changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -115,4 +115,22 @@ router.get('/user/:user_id', async (req, res) => {
         res.status(500).send("Server Error");
     }
 });
+// @route Delete api/profile
+// @desc Delete profiles, user & posts
+// Access Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // Remove users posts
+
+        // Remove profile
+        await Profile.findByIdAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findByIdAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted' });
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 module.exports = router;
